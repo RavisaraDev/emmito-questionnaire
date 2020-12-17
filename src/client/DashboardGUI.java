d4884307572774f8156b0c708e7e55e036d0bf18
ravisara <ravisaranipun97@gmail.com>
2020-12-17 13:49:09 +0530

    Dashboard header banner

--- src/client/DashboardGUI.java
+++ src/client/DashboardGUI.java
@@ -22,6 +22,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
 import javax.swing.ImageIcon;
+import javax.swing.LayoutStyle.ComponentPlacement;
 
 // TODO: Auto-generated Javadoc
 /**
@@ -129,16 +130,6 @@ public class DashboardGUI extends JFrame {
 		button_2.setForeground(Color.WHITE);
 		button_2.setFont(new Font("Dialog", Font.BOLD, 23));
 		button_2.setBackground(new Color(123, 104, 238));
-
-		JLabel label = new JLabel("Feedback Portal");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setForeground(Color.WHITE);
-		label.setFont(new Font("Dialog", Font.BOLD, 40));
-
-		JLabel label_1 = new JLabel("Welcome to emmito");
-		label_1.setHorizontalAlignment(SwingConstants.CENTER);
-		label_1.setForeground(Color.WHITE);
-		label_1.setFont(new Font("Dialog", Font.BOLD, 40));
 		
 		JLabel lblNewLabel = new JLabel(cookie == null ? "" : "⬤ Logged in as admin");
 		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 25));
@@ -148,7 +139,11 @@ public class DashboardGUI extends JFrame {
 		lblNewLabel_1.setIcon(new ImageIcon("/home/nipun/eclipse-workspace/EmmitoUserFeedbackQuestionnaire/src/assets/bg.png"));
 		GroupLayout gl_panel = new GroupLayout(panel);
 		gl_panel.setHorizontalGroup(
-			gl_panel.createParallelGroup(Alignment.LEADING)
+			gl_panel.createParallelGroup(Alignment.TRAILING)
+				.addGroup(gl_panel.createSequentialGroup()
+					.addGap(266)
+					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addGap(271))
 				.addGroup(gl_panel.createSequentialGroup()
 					.addGap(194)
 					.addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
@@ -156,48 +151,32 @@ public class DashboardGUI extends JFrame {
 						.addComponent(button_1, GroupLayout.DEFAULT_SIZE, 473, Short.MAX_VALUE)
 						.addComponent(button_2, GroupLayout.DEFAULT_SIZE, 473, Short.MAX_VALUE))
 					.addGap(185))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label, GroupLayout.DEFAULT_SIZE, 828, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(gl_panel.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(label_1, GroupLayout.DEFAULT_SIZE, 828, Short.MAX_VALUE)
-					.addContainerGap())
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(273)
-					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(264))
-				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(247)
-					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 411, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(194, Short.MAX_VALUE))
+				.addComponent(lblNewLabel_1, Alignment.LEADING, GroupLayout.PREFERRED_SIZE, 824, Short.MAX_VALUE)
 		);
 		gl_panel.setVerticalGroup(
 			gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup()
-					.addGap(56)
-					.addComponent(label_1)
-					.addGap(18)
-					.addComponent(label, GroupLayout.PREFERRED_SIZE, 55, GroupLayout.PREFERRED_SIZE)
-					.addGap(32)
+					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
+					.addGap(189)
 					.addComponent(lblNewLabel, GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
-					.addGap(128)
+					.addGap(32)
 					.addComponent(button, GroupLayout.PREFERRED_SIZE, 71, GroupLayout.PREFERRED_SIZE)
 					.addGap(36)
 					.addComponent(button_2, GroupLayout.PREFERRED_SIZE, 63, GroupLayout.PREFERRED_SIZE)
 					.addGap(39)
 					.addComponent(button_1, GroupLayout.PREFERRED_SIZE, 78, GroupLayout.PREFERRED_SIZE)
-					.addGap(152)
-					.addComponent(lblNewLabel_1, GroupLayout.PREFERRED_SIZE, 116, GroupLayout.PREFERRED_SIZE)
-					.addGap(81))
+					.addGap(349))
 		);
 		panel.setLayout(gl_panel);
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(panel,
-				GroupLayout.DEFAULT_SIZE, 824, Short.MAX_VALUE));
-		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addComponent(panel,
-				Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 1035, Short.MAX_VALUE));
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, GroupLayout.PREFERRED_SIZE, 824, Short.MAX_VALUE)
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addComponent(panel, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+		);
 		contentPane.setLayout(gl_contentPane);
 	}
 }
